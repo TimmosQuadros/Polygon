d75e1aaed2f3e1381f4bb88c081a8bb9eb724b27
EidolonRED <peter2137@gmail.com>
2016-03-30 11:26:39 +0200

    SQL updated

--- Polygon/SQL/Dump20160330.sql
+++ Polygon/SQL/Dump20160330.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `buildings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `buildings` (
-  `building_id` decimal(8,0) NOT NULL,
+  `building_id` int(11) NOT NULL AUTO_INCREMENT,
   `building_name` varchar(32) DEFAULT NULL,
   `street_address` varchar(64) DEFAULT NULL,
   `zipcode` decimal(8,0) DEFAULT NULL,
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-30 10:28:05
+-- Dump completed on 2016-03-30 11:25:56
