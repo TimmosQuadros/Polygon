496929f29cf071b84e7458f30a3a124649eebd01
Waqas <waqas.mohiuddin@live.com>
2016-03-30 11:09:41 +0200

    updated constructer for building class

--- Polygon/src/data/Building.java
+++ Polygon/src/data/Building.java
@@ -5,6 +5,17 @@ class Building {
 	String building_name, street_address;
 	int building_id, zip, build_year, floor_area;
 	
+	public Building(String building_name, String street_address, int building_id, int zip, int build_year,
+			int floor_area) {
+		super();
+		this.building_name = building_name;
+		this.street_address = street_address;
+		this.building_id = building_id;
+		this.zip = zip;
+		this.build_year = build_year;
+		this.floor_area = floor_area;
+	}
+
 	public int getBuild_year() {
 		return build_year;
 	}
