b6669fa56bf608b815f453bf1101e480acf4fc5d
Waqas <waqas.mohiuddin@live.com>
2016-03-30 10:35:28 +0200

    added Building.java and removed comment

---
+++ Polygon/src/Data/Building.java
@@ -0,0 +1,32 @@
+package Data;
+
+class Building {
+	int building_id, zip;
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	String building_name, street_address;
+	public int getBuilding_id() {
+		return building_id;
+	}
+	public void setBuilding_id(int building_id) {
+		this.building_id = building_id;
+	}
+	public String getBuilding_name() {
+		return building_name;
+	}
+	public void setBuilding_name(String building_name) {
+		this.building_name = building_name;
+	}
+	public String getStreet_address() {
+		return street_address;
+	}
+	public void setStreet_address(String street_address) {
+		this.street_address = street_address;
+	}
+	
+	
+}
