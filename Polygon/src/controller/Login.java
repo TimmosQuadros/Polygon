16bef2ea7c654a2ccb1dce87a30f6f5e6b2f4028
Waqas <waqas.mohiuddin@live.com>
2016-03-30 09:55:18 +0200

    just added new comment

--- Polygon/src/controller/Login.java
+++ Polygon/src/controller/Login.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
+
+// abe
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
