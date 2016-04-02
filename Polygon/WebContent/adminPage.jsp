41faa7c6b71dfd09c9b54fa4959cfe1f72cb6f29
rasmus <rasmuspuls@gmail.com>
2016-04-02 11:06:02 +0200

    admin jsp og servlet

---
+++ Polygon/WebContent/adminPage.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Administrator control panel</title>
+<style>
+.clickMe {
+    -moz-appearance: button;
+    -ms-appearance: button;
+    -o-appearance: button;
+    -webkit-appearance: button;
+    appearance: button;
+    text-decoration: none;
+    color: #000;
+    padding: 0.2em 0.4em;
+}​
+</style>
+</head>
+<body>
+
+
+
+</body>
+</html>
---
+++ Polygon/src/controller/adminPage.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import data.UserMapper;
+import data.BuildingMapper;
+import data.OrganisationMapper;
+
+/**
+ * Servlet implementation class adminPage
+ */
+@WebServlet("/adminPage")
+public class adminPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	UserMapper uMap;
+	BuildingMapper bMap;
+	OrganisationMapper oMap;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public adminPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
