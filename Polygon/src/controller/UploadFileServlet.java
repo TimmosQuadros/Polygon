b0ed094b3931f8b3813dd05f294b334e1cee90e0
TimmosQuadros <timm.rasmussen@gmail.com>
2016-04-11 12:11:36 +0200

    New upload method test.

--- Polygon/src/controller/UploadFileServlet.java
+++ Polygon/src/controller/UploadFileServlet.java
@@ -3,10 +3,8 @@ package controller;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,9 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.apache.tomcat.util.http.fileupload.RequestContext;
 import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
 
 /**
  * Servlet implementation class UploadFileServlet
@@ -37,79 +37,31 @@ public class UploadFileServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final int MAXSIZE = 5000*1024;
 		
 		PrintWriter out = response.getWriter();
+		DiskFileItemFactory factory = new DiskFileItemFactory(MAXSIZE, new File("c:/temp"));
+
+		ServletFileUpload upload = new ServletFileUpload(factory);
 		
-		// TODO Auto-generated method stub
-		File file;
-		int maxFileSize = 5000 * 1024;
-		int maxMemSize = 5000 * 1024;
+		upload.setSizeMax(MAXSIZE);
 
-		ServletContext context = request.getServletContext();
-		String filePath = context.getInitParameter("file-upload");
-
-		// Verify the content type
-		String contentType = request.getContentType();
-		if((contentType.indexOf("multipart/form-data")>=0)){
-			DiskFileItemFactory factory = new DiskFileItemFactory();
-			// maximum size that will be stored in memory
-			factory.setSizeThreshold(maxMemSize);
-			// Location to save data that is larger than maxMemSize.
-			factory.setRepository(new File("c:\\temp"));
-			// Create a new file upload handler
-			ServletFileUpload upload = new ServletFileUpload(factory);
-			// maximum file size to be uploaded.
-			upload.setSizeMax(maxFileSize);
-			
-			try{ 
-		         // Parse the request to get file items.
-		         List fileItems = upload.parseRequest((RequestContext) request);
-
-		         // Process the uploaded file items
-		         Iterator i = fileItems.iterator();
-
-		         out.println("<html>");
-		         out.println("<head>");
-		         out.println("<title>JSP File upload</title>");  
-		         out.println("</head>");
-		         out.println("<body>");
-		         while ( i.hasNext () ) 
-		         {
-		            FileItem fi = (FileItem)i.next();
-		            if ( !fi.isFormField () )	
-		            {
-		            // Get the uploaded file parameters
-		            String fieldName = fi.getFieldName();
-		            String fileName = fi.getName();
-		            boolean isInMemory = fi.isInMemory();
-		            long sizeInBytes = fi.getSize();
-		            // Write the file
-		            if( fileName.lastIndexOf("\\") >= 0 ){
-		            file = new File( filePath + 
-		            fileName.substring( fileName.lastIndexOf("\\"))) ;
-		            }else{
-		            file = new File( filePath + 
-		            fileName.substring(fileName.lastIndexOf("\\")+1)) ;
-		            }
-		            fi.write( file ) ;
-		            out.println("Uploaded Filename: " + filePath + 
-		            fileName + "<br>");
-		            }
-		         }
-		         out.println("</body>");
-		         out.println("</html>");
-		      }catch(Exception ex) {
-		         System.out.println(ex);
-		      }
-		   }
-		}
-
-		/**
-		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-			doGet(request, response);
+		// Parse the request
+		try {
+			List<FileItem> items = upload.parseRequest((RequestContext) request);
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
