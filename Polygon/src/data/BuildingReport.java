f9e87bae37822e06672bfaa693bb3a83d29d92cf
EidolonRED <peter2137@gmail.com>
2016-04-11 10:22:37 +0200

    New report classes

---
+++ Polygon/src/data/BuildingReport.java
@@ -0,0 +1,17 @@
+package data;
+
+import java.util.ArrayList;
+
+import org.apache.tomcat.jni.File;
+
+public class BuildingReport {
+
+	private int buildingID;
+	private String roofRemark;
+	private File roofImage;
+	private String outerWallRemark;
+	private File outerWallImage;
+	private ArrayList<RoomReport> roomReports;
+	
+	
+}
