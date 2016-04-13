ad251c8e39f0012faed99f5991d07970e8fdd926
rasmus <rasmuspuls@gmail.com>
2016-04-13 11:53:57 +0200

    condition level added

--- Polygon/WebContent/Resources/js/healtReport.js
+++ Polygon/WebContent/Resources/js/healtReport.js
@@ -18,3 +18,15 @@ $(document).ready(function(){
         $("#outerWalls").show();
     });
 });
+
+//script til checkbox (condition level) 
+$("input:checkbox").on('click', function() {
+	  var $box = $(this);
+	  if ($box.is(":checked")) {
+	    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+	    $(group).prop("checked", false);
+	    $box.prop("checked", true);
+	  } else {
+	    $box.prop("checked", false);
+	  }
+	});
