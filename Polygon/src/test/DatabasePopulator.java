35aaf852d4f2a357c3693595b8f8cc779b013666
EidolonRED <peter2137@gmail.com>
2016-04-14 11:43:50 +0200

    checkups added to populator

--- Polygon/src/test/DatabasePopulator.java
+++ Polygon/src/test/DatabasePopulator.java
@@ -3,6 +3,7 @@ package test;
 import java.sql.SQLException;
 
 import data.Building;
+import data.Checkup;
 import data.Facade;
 import data.User;
 
@@ -16,6 +17,7 @@ public class DatabasePopulator {
 		fac.createUser(new User(User.User_type.TECH, "testtech", "123456", "tech@mail.com"), "testorg1");
 		for (int i = 1; i <= 3; i++) {
 			fac.createBuilding(new Building("testbuilding".concat(String.valueOf(i)), "testaddress".concat(String.valueOf(i)), (340 + i) * 10, 1990 + i, i * 40), 2);
+			fac.createCheckup(new Checkup(i, i, "2016-04-14"));
 		}
 	}
 
