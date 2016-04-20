4fcb8143d0c82745cd047e627e9d676f7d3df23c
TimmosQuadros <timm.rasmussen@gmail.com>
2016-04-20 11:49:19 +0200

    Added exception handling to CreateBuildingServlet.

--- Polygon/src/controller/CreateBuildingServlet.java
+++ Polygon/src/controller/CreateBuildingServlet.java
@@ -2,6 +2,9 @@ package controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import data.Building;
 import data.Facade;
 import data.User;
+
 
 @WebServlet("/CreateBuilding")
 public class CreateBuildingServlet extends HttpServlet {
@@ -27,41 +31,48 @@ public class CreateBuildingServlet extends HttpServlet {
 		super.init();
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		HttpSession session = request.getSession(true);
-		String building_name = request.getParameter("building_name");
-		String street_address = request.getParameter("street_address");
-		int zip = Integer.parseInt(request.getParameter("zipcode"));
-		int build_year = Integer.parseInt(request.getParameter("build_year"));
-		int floor_area = Integer.parseInt(request.getParameter("floor_area"));
 		User user = (User)session.getAttribute("user");
 		if(user!=null){
 			try {
+				String building_name = request.getParameter("building_name");
+				String street_address = request.getParameter("street_address");
+				int zip = Integer.parseInt(request.getParameter("zipcode"));	
+				int build_year = Integer.parseInt(request.getParameter("build_year"));
+				int floor_area = Integer.parseInt(request.getParameter("floor_area"));
 				facade.createBuilding(new Building(building_name, street_address, zip, build_year, floor_area),user.getOrganisations_id());
+				facade.createFloorplan(facade.getMaxImageId(), facade.getMaxBuildingId());
 			} catch (SQLException e) {
-				e.printStackTrace();
+				response.getWriter().append("<script language=\"javascript\"> "
+						+ "alert( \"An error occurred either: yoou tried to add a building with a name that already exists.\" ); </script>");
+				forward(request, response, "/addBuilding.jsp");
 			}catch (NumberFormatException e) {
-				e.printStackTrace();
+				response.getWriter().append("<script language=\"javascript\"> "
+						+ "alert( \"You have enterede wrong format in some of the fields.\" ); </script>");
+				forward(request, response, "/addBuilding.jsp");
 			}catch (NullPointerException e) {
-				e.printStackTrace();
+				response.getWriter().append("<script language=\"javascript\"> "
+						+ "alert( \"An internal null pointer error happend try to login again.\" ); </script>");
+				forward(request, response, "/addBuilding.jsp");
 			}catch (Exception e) {
-				e.printStackTrace();
+				response.getWriter().append("<script language=\"javascript\"> "
+						+ "alert( \"An internal error happende contact admin.\" ); </script>");
+				forward(request, response, "/addBuilding.jsp");
 			}
 		}
-
-		try {
-			//keep it in the session instead. 
-			if(true){facade.createFloorplan(facade.getMaxImageId(), facade.getMaxBuildingId());}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+	}
+	
+	private void forward(HttpServletRequest req, HttpServletResponse res, String path) throws IOException, ServletException {
+		ServletContext sc = getServletContext();
+		RequestDispatcher rd = sc.getRequestDispatcher(path);
+		rd.forward(req, res);
 	}
 
 }
